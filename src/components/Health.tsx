173a081429c424a72521cb3a02ec45fb0233e371
Alan Van Dyke <alan.m.vandyke@gmail.com>
2024-03-24 18:18:54 -0500

    health and hit dice

--- src/components/Health.tsx
+++ src/components/Health.tsx
@@ -1,9 +1,46 @@
-import "../style/Health.css"
+import "../style/Health.css";
+import { useCharacterContext } from "../context/CharacterContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faMinus,
+  faShieldHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Health = () => {
-  return (
-    <div className="health-container">Health</div>
-  )
-}
+  const { state, dispatch } = useCharacterContext();
 
-export default Health
+  return (
+    <div className="health-container">
+      <div className="hitpoints-top-bar">
+        <div className="max-hitpoints-bar">
+          <p className="max-hitpoints-label">
+            <i>Max Hit Points: </i>
+          </p>
+          <p className="max-hitpoints-value">{state.maxHitPoints}</p>
+        </div>
+        <div className="health-btn-container">
+          <button
+            className="health-btn btn-temp"
+            title="Add temporary hit points"
+          >
+            <FontAwesomeIcon icon={faShieldHeart} />
+          </button>
+          <button className="health-btn btn-heal" title="Heal">
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+          <button className="health-btn btn-damage" title="Take damage">
+            <FontAwesomeIcon icon={faMinus} />
+          </button>
+        </div>
+      </div>
+      <h4>Current Hit Points</h4>
+      <h1>{state.currentHitPoints}</h1>
+      <hr></hr>
+      <h4>Temporary Hit Points</h4>
+      <h1>{state.tempHitPoints}</h1>
+    </div>
+  );
+};
+
+export default Health;
--- src/components/HitDice.tsx
+++ src/components/HitDice.tsx
@@ -1,7 +1,70 @@
-import "../style/HitDice.css"
+import "../style/HitDice.css";
+import { useCharacterContext } from "../context/CharacterContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const HitDice = () => {
-  return <div className="hit-dice-container">HitDice</div>;
+  const { state, dispatch } = useCharacterContext();
+
+  return (
+    <div className="hit-dice-container">
+      <h4>Hit Dice</h4>
+      <hr className="hit-dice-divider"></hr>
+      <div className="hit-dice-list">
+        {state.hitDice.map((hitDie, index) => {
+          if (hitDie.max > 0) {
+            return (
+              <div className="hit-die-list-item" key={"hit-die-" + index}>
+                <p>{hitDie.type}</p>
+                <div className="hit-die-value-group">
+                  <p>{hitDie.current + "/" + hitDie.max}</p>
+                  <button
+                    className="hit-die-btn btn-up"
+                    onClick={(event) => {
+                      if (hitDie.current < hitDie.max) {
+                        dispatch({
+                          type: "CHANGE_HIT_DICE",
+                          payload: {
+                            type: hitDie.type,
+                            newCurrent: hitDie.current + 1,
+                            newMax: hitDie.max,
+                          },
+                        });
+                      }
+                    }}
+                    title="Restore"
+                  >
+                    <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                  </button>
+                  <button
+                    className="hit-die-btn btn-down"
+                    onClick={(event) => {
+                      console.log(
+                        "TRYING " + hitDie.current + " on " + hitDie.type
+                      );
+                      if (hitDie.current > 0) {
+                        dispatch({
+                          type: "CHANGE_HIT_DICE",
+                          payload: {
+                            type: hitDie.type,
+                            newCurrent: hitDie.current - 1,
+                            newMax: hitDie.max,
+                          },
+                        });
+                      }
+                    }}
+                    title="Spend"
+                  >
+                    <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default HitDice;
